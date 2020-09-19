634a274ab08a4db38386bbcf9dd29f3ca72145e5
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2020-09-19 15:29:11 +0530

    Fixed Jumping and other movement script changes

--- covid_19_jam/nodes/player/player.gd
+++ covid_19_jam/nodes/player/player.gd
@@ -12,35 +12,41 @@ var interface
 var energyball
 var jumped
 onready var cam=$cam
+
+var move_vec=Vector3(0, 0, 0)
+
 func _ready(): 
 	pass
+
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		cam.rotation_degrees.x -= event.relative.y * V_look_sens
 		cam.rotation_degrees.x =clamp(cam.rotation_degrees.x,-45,90)
 		rotation_degrees.y -= event.relative.x * M_LOOK_SENS
+	
 
-func _physics_process(_delta):
-	var move_vec=Vector3()
-	var idle=true
+func usr_input():
+	
+	move_vec = Vector3()
+	# Take in put from user
 	if Input.is_action_pressed("move_forward"):
-		idle=false
 		move_vec.z-=1
-		MOVE_SPEED=16
 	if Input.is_action_pressed("move_backward"):
-		idle=false
 		move_vec.z+=1
-		MOVE_SPEED=11
 	if Input.is_action_pressed("move_right"):
-		idle=false
 		move_vec.x+=1
-		MOVE_SPEED=12
 	if Input.is_action_pressed("move_left"):
 		print("left")
-		idle=false
 		move_vec.x-=1
-		MOVE_SPEED=12
 
+
+func _physics_process(delta):
+
+	usr_input()
+	
+	print(move_vec)
+	
 	move_vec= move_vec.normalized()
 	move_vec=move_vec.rotated(Vector3(0,1,0),rotation.y)
 	move_vec*= MOVE_SPEED
@@ -49,28 +55,10 @@ func _physics_process(_delta):
 	
 	
 	var grounded = is_on_floor()
-	y_vel-=gravity
+	if !grounded:
+		y_vel-=gravity
 	if grounded and Input.is_action_just_pressed("jump"):
-		jumped=true
 		y_vel= JUMP_FORCE
-	if grounded and y_vel<0:
-		jumped=false
-		y_vel =0
-		
-	if y_vel < -max_fall_speed:
-		y_vel= -max_fall_speed
 	
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
+	#if y_vel < -max_fall_speed:
+	#	y_vel= -max_fall_speed
