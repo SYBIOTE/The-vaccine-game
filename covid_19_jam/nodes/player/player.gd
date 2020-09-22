ecb92319113b71d0d31f4e680bd79d2013873f98
rahulghosh <ghoshr698@gmail.com>
2020-09-22 17:57:33 +0530

    the ui and hints

--- covid_19_jam/nodes/player/player.gd
+++ covid_19_jam/nodes/player/player.gd
@@ -10,7 +10,8 @@ var jumped
 var scale_factor=.03
 var scale_mult=0
 onready var cam=$cam
-
+onready var ray=$cam/RayCast
+onready var hintbar=$Control/HintBar
 var move_vec=Vector3(0, 0, 0)
 
 
@@ -23,7 +24,7 @@ func _input(event):
 		cam.rotation_degrees.x =clamp(cam.rotation_degrees.x,-50,90)
 		rotation_degrees.y -= event.relative.x * M_LOOK_SENS
 #		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
+		
 
 func usr_input():
 	
@@ -41,18 +42,39 @@ func usr_input():
 		scale_mult=-1
 	if Input.is_action_just_released("crouch"):
 		scale_mult=+1
+func sight():
+	var l_o_s = ray.get_collider()
+	if ray.is_colliding():
+#		print(l_o_s.name)
+		match l_o_s.name:
+			"door":
+#				print("door detected")
+				hintbar.show_hint("go to work?")
+			"sanitiser":
+#				print("sanitizer detected")
+				hintbar.show_hint("use sanitizer?")
+			"mask":
+#				print("mask detected")
+				hintbar.show_hint("wear_mask?")
+			"bed":
+#				print("bed detected")
+				hintbar.show_hint("call it a day?")
+			"computer":
+#				print("computer detected")
+				hintbar.show_hint("take a leave?")
+	else:
+		hintbar.reset()
 func _physics_process(delta):
 	
 	usr_input()
 	crouch()
-#	print(move_vec)
-	print(scale.y)
+	
 	move_vec= move_vec.normalized()
 	move_vec=move_vec.rotated(Vector3(0,1,0),rotation.y)
 	move_vec*= MOVE_SPEED
 	move_vec.y = y_vel
 	move_and_slide(move_vec,Vector3(0,1,0))
-	
+	sight()
 	
 #	var grounded = is_on_floor()
 #	if !grounded:
--- covid_19_jam/scenes/ui/HintBar.gd
+++ covid_19_jam/scenes/ui/HintBar.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 func show_hint(_hint):
+	$ColorRect/HintLabel.show()
 	$ColorRect/HintLabel.text = _hint
 func reset():
-	$ColorRect/HintLabel.text = "Use WASD to move around..."
+	$ColorRect/HintLabel.hide()
