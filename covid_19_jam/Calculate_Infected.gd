622485eab37607d53d6b65a2f37fd1dc0a98cb3d
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2020-09-23 17:26:38 +0530

    Started implementation, calculateInfected working

--- covid_19_jam/Calculate_Infected.gd
+++ covid_19_jam/Calculate_Infected.gd
@@ -1,33 +1,41 @@
 extends Spatial
 
-public int totalPopulation
-public int totalInfected
-public int totalHealthy
+var random = RandomNumberGenerator.new()
 
-public int infectionRiskPercent
-private float percentOfTotalInfected
+var totalPopulationNum = 200
+var totalInfected = 10
+var totalHealthy = 0
+
+var infectionRiskPercent
+var percentOfTotalInfected
+
 
 # This function will do things when this node is is put in the scene
 func _ready():
-	totalPopulation = 250
-	int percentageOfGeneralPopulationInfected = 8
-	percentageOfTotalInfected = percentageOfGeneralPopulationInfected
+	totalPopulationNum = 250
+	random.randomize()
 
+	totalInfected *= 100
+	percentOfTotalInfected = totalInfected/totalPopulationNum
+	totalInfected /= 100
 
-# This function will do all the calculations
-func _process():
-	pass
+	print("Initial percentOfTotalInfected: " + str(percentOfTotalInfected))
+
+	calculate_ans()
 
 func calculate_ans():
-	int i = 0
-	while i < totalPopulation:
-		int infectionRoll = random.randi_range(101)
+	var i = 0
+	while i < totalPopulationNum:
+		var infectionRoll = random.randi_range(1,101)
+		#print(str(i) + ": " + "infectionRoll: " + str(infectionRoll) + " ; percentOfTotalInfected: " + str(percentOfTotalInfected))
 
-		if infectionRoll < percentageOfTotalInfected:
+		if infectionRoll < percentOfTotalInfected:
 			totalInfected += 1
 		else:
 			totalHealthy += 1
 
-		percentageOfTotalInfected = (totalInfected / totalPopulation) * 100
-
 		i += 1
+
+	totalInfected *= 100
+	percentOfTotalInfected = totalInfected/totalPopulationNum
+	print("percentOfTotalInfected: " + str(percentOfTotalInfected))
--- covid_19_jam/PlayerData.gd
+++ covid_19_jam/PlayerData.gd
@@ -1,23 +1,18 @@
 extends Spatial
 
-bool isWearingMask #Wearing a mask is a really important factor in deciding the totalRisk.
-int infectionRisk #The risk of infection, this changes depending upon where the player is and also how clean he is.
-int transmissionRisk #The risk of the disease betting transmitted from one person to another.
-int cleanliness #How clean the player keeps itself, more the better. but too much is harmful to health
-int workLoad #The amount of work the player does, needed to make the vaccine, but too much and health declines.
+var isWearingMask #Wearing a mask is a really important factor in deciding the totalRisk.
+var infectionRisk #The risk of infection, this changes depending upon where the player is and also how clean he is.
+var transmissionRisk #The risk of the disease betting transmitted from one person to another.
+var cleanliness #How clean the player keeps itself, more the better. but too much is harmful to health
+var workLoad #The amount of work the player does, needed to make the vaccine, but too much and health declines.
 # too less and the player gets kicked and dies of poor health as he/she becomes is unemployed and homeless.
-int lackOfInteraction  #This is the amount of interaction the player has with other people
+var lackOfInteraction  #This is the amount of interaction the player has with other people
 
-int health
+var health
 
-int totalRisk #An accumulation of all the factors like wearing mask, cleanliness, health, etc.
+var totalRisk #An accumulation of all the factors like wearing mask, cleanliness, health, etc.
 # also the final deciding factor as to wether the player gets infected or not ( can never be 0 )
 
-func _ready():
-	pass
-
-func _process():
-	pass
 
 func calc_totalRisk(infectionRisk, transmissionRisk, cleanliness, health, isWearingMask, totalRisk):
 	totalRisk = (infectionRisk + transmissionRisk - cleanliness - health)/2
