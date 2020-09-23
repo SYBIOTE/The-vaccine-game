afd1aefeb3f53308e7729021e5e6ec0e3b353de6
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2020-09-23 18:27:45 +0530

    Got world data working

--- covid_19_jam/Calculate_Infected.gd
+++ covid_19_jam/Calculate_Infected.gd
@@ -1,19 +1,26 @@
 extends Spatial
 
 var random = RandomNumberGenerator.new()
+onready var data = get_parent().get_node("SaveAndLoadData")
 
-var totalPopulationNum = 200
-var totalInfected = 10
-var totalHealthy = 0
+var totalPopulationNum
+var totalInfected
+var totalHealthy
 
 var infectionRiskPercent
 var percentOfTotalInfected
 
-
 # This function will do things when this node is is put in the scene
 func _ready():
+
+	print(data.wdata)
+	var world_data = data.wdata
+
 	totalPopulationNum = 250
 	random.randomize()
+
+	totalInfected = 10
+	totalHealthy = 0
 
 	totalInfected *= 100
 	percentOfTotalInfected = totalInfected/totalPopulationNum
@@ -21,9 +28,10 @@ func _ready():
 
 	print("Initial percentOfTotalInfected: " + str(percentOfTotalInfected))
 
-	calculate_ans()
+	calculate_ans(world_data)
+	calculate_ans(world_data)
 
-func calculate_ans():
+func calculate_ans(var world_data):
 	var i = 0
 	while i < totalPopulationNum:
 		var infectionRoll = random.randi_range(1,101)
@@ -31,11 +39,20 @@ func calculate_ans():
 
 		if infectionRoll < percentOfTotalInfected:
 			totalInfected += 1
-		else:
-			totalHealthy += 1
 
 		i += 1
 
 	totalInfected *= 100
 	percentOfTotalInfected = totalInfected/totalPopulationNum
+	totalInfected /= 100
+
+	totalHealthy = totalPopulationNum - totalInfected
+
 	print("percentOfTotalInfected: " + str(percentOfTotalInfected))
+	world_data["percentOfTotalInfected"] = percentOfTotalInfected
+	world_data["totalInfected"] = totalInfected
+	world_data["totalHealthy"] = totalHealthy
+	world_data["totalPopulation"] = totalPopulationNum
+
+	data.save_data(world_data, data.save_wdata)
+	print(world_data)
--- covid_19_jam/SaveAndLoadData.gd
+++ covid_19_jam/SaveAndLoadData.gd
@@ -1,9 +1,10 @@
 extends Spatial
 
-var save_path = "user://save_temp.dat"
+var save_pdata = "user://save_pdata.dat"
+var save_wdata = "user://save_wdata.dat"
 
 # Player data
-var pdata = {
+export var pdata = {
 		"name" : "",
 		"health" : 0,
 		"totalRisk" : 0,
@@ -16,37 +17,37 @@ var pdata = {
 		}
 
 # World data
-var wdata = {
+export var wdata = {
 		"totalPopulation" : 0,
 		"totalInfected" : 0,
 		"totalHealthy" : 0,
 		"infectionRiskPercent" : 0,
-		"percentageOfTotalInfected" : 0,
+		"percentOfTotalInfected" : 0,
 	}
 
-func save_data(var dt = null):
+func save_data(var dt = null, var path = null):
 	var file = File.new()
-	var error = file.open(save_path, File.WRITE)
+	var error = file.open(path, File.WRITE)
 	if error == OK:
 		file.store_var(dt)
 		file.close()
 
-func load_pdata(var dt = null):
+func load_pdata():
 	var file = File.new()
 	var p_data = null
-	if file.file_exists(save_path):
-		var error = file.open(save_path, File.READ)
+	if file.file_exists(save_pdata):
+		var error = file.open(save_pdata, File.READ)
 		print(error)
 		if error == OK:
 			p_data = file.get_var()
 			file.close()
 	return p_data
 
-func load_wdata(var dt = null):
+func load_wdata():
 	var file = File.new()
 	var w_data = null
-	if file.file_exists(save_path):
-		var error = file.open(save_path, File.READ)
+	if file.file_exists(save_wdata):
+		var error = file.open(save_wdata, File.READ)
 		print(error)
 		if error == OK:
 			w_data = file.get_var()
